8739282b7650c49aa08628dbebf85db5fc117a3f
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-07-19 14:06:01 +0530

    modified the git ignore

--- src/main/java/com/harshan92/bookstore/BookstoreApplication.java
+++ src/main/java/com/harshan92/bookstore/BookstoreApplication.java
@@ -1,16 +1,22 @@
 package com.harshan92.bookstore;
 
 import com.harshan92.bookstore.domain.User;
+import com.harshan92.bookstore.domain.security.Role;
+import com.harshan92.bookstore.domain.security.UserRole;
 import com.harshan92.bookstore.service.UserService;
+import com.harshan92.bookstore.utility.SecurityUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @SpringBootApplication
 public class BookstoreApplication implements CommandLineRunner {
 	@Autowired
-	private UserService;
+	private UserService userService;
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
@@ -18,6 +24,16 @@ public class BookstoreApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		User user1=new User();
-
+		user1.setFirstname("John");
+		user1.setLastname("Doe");
+		user1.setUsername("johndoe");
+		user1.setPassword(SecurityUtility.passwordEncoder().encode("p"));
+		user1.setEmail("johndoe@mail.com");
+		Set<UserRole> userRoles=new HashSet<>();
+		Role role1=new Role();
+		role1.setRoleId(1);
+		role1.setName("ROLE_USER");
+		userRoles.add(new UserRole(user1, role1));
+		userService.createUser(user1, userRoles);
 	}
 }
